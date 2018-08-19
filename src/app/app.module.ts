9345a62da918a6ce72b732961ee467a508c10e9a
Phabiann-Ross De Carvalho <cervantes1234@live.com>
2018-08-19 22:55:39 +0200

    Still setting up a decent style structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
