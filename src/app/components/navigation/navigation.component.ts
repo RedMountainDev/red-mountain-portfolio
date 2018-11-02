4d5b21aa89f851cfbb241ce9ee86310382543c4e
Phabiann-Ross De Carvalho <cervantes1234@live.com>
2018-11-02 20:25:05 +0200

    reworked the responsive behaviour of the navbar and added social button links

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {log} from 'util';
+import {faGithubSquare, faFacebookSquare, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-navigation',
@@ -11,6 +10,9 @@ export class NavigationComponent implements OnInit {
 
 
   routerOptions = {exact: true};
+  githubIcon = faGithubSquare;
+  facebookIcon = faFacebookSquare;
+  linkedInIcon = faLinkedin;
 
   constructor() {
   }
